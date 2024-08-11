262554909a149e3177dc978bf29871ba44eee722
jguacide <guacidejohanna@gmail.com>
2024-08-11 13:53:31 +0200

    all function calls tested, pipe_token works

--- src/tokenizer/tokenizer.c
+++ src/tokenizer/tokenizer.c
@@ -11,32 +11,29 @@ int	get_token_hint(char c)
 			break;
 		value++;
 	}
-	if (value == 6) // if c is not in set_type[] will return 7
-		value += 1;
-	return (value);
+	return (value); // if c is not in set_type[] will return 6 (WORD)
 }
 
-void	fill_token_info(int *current_pos, char *input_str, t_token **new_token)
+void	fill_token_info(int *current_pos, char *input_str, t_token *new_token)
 {
 	int	start_pos;
 	int	token_hint;
 
 	start_pos = *current_pos;
-	token_hint = get_token_hint(input_str[*current_pos]);
-	void (*get_full_token[])(int *, char *, char, t_token **) =
+	new_token->type = get_token_hint(input_str[*current_pos]);
+	void (*get_full_token[])(int *, char *, char, t_token *) =
 	{
 		[TOKEN] = NULL,
 		[PIPE] = pipe_token,
 		[S_QUOTE] = NULL,
 		[D_QUOTE] = NULL,
-		[REDIRECT_IN] = NULL,
+		// [REDIRECT_IN] = redirect_in_token,
 		[REDIRECT_OUT] = NULL,
-		[REDIRECT_OUT_APPEND] = NULL, // TODO: int this function set token_id 
 		[WORD] = NULL
 	};
-	get_full_token[token_hint](&current_pos, input_str[*current_pos], &new_token) // TODO: Do i send input_str in total or just at a certain position
-	(*new_token)->str = ft_substr(input_str, start_pos, ((*current_pos - start_pos) + 1));
-	printf("%s", (*new_token)->str);
+	get_full_token[new_token->type](current_pos, input_str, input_str[*current_pos], new_token);
+	// (*new_token)->str = ft_substr(input_str, start_pos, ((*current_pos - start_pos) + 1));
+	// printf("%s", (*new_token)->str);
 
 }
 
@@ -52,7 +49,7 @@ t_token	*extract_tokens(char *input_str)
 	while (input_str[current_pos] != '\0')
 	{
 		new_token = create_new_token();
-		fill_token_info(&current_pos, input_str, &new_token);
+		fill_token_info(&current_pos, input_str, new_token);
 		token_list_add_back(&token_list_head, new_token);
 		current_pos++;
 	}
--- src/tokenizer/tokenizer_utils.c
+++ src/tokenizer/tokenizer_utils.c
@@ -1,15 +1,43 @@
 #include "../../includes/minishell.h"
 
-void	pipe_token(int *current_pos, char *input_str, char token_hint, t_token **new_token)
+void	pipe_token(int *current_pos, char *input_str, char c, t_token *new_token)
 {
 	int i;
 	if (!input_str)
 		return ;
 	i = 0;
-	while (i < 2 && input_str[*current_pos] && input_str[*current_pos] == token_hint)
+	while (i < 2 && input_str[*current_pos] && input_str[*current_pos] == c)
 	{
 		(*current_pos)++;
 		i++;
 	}
-	(*new_token)->type = PIPE;
+	new_token->type = PIPE;
+}
+//
+// void	redirect_in_token(int *current_pos, char *input_str, char token_hint, t_token **new_token)
+// {
+// 	int i;
+// 	if (!input_str)
+// 		return ;
+// 	i = 0;
+// 	while (i < 2 && input_str[*current_pos] && input_str[*current_pos] == token_hint)
+// 	{
+// 		(*current_pos)++;
+// 		i++;
+// 	}
+// 	(*new_token)->type = REDIRECT_IN;
+// } //TODO : what about << ? what does it mean? if i handle it, should i create a separate token id for it?
+//
+void	single_quote_token(int *current_pos, char *input_str, char token_hint, t_token **new_token)
+{
+	int i;
+	if (!input_str)
+		return ;
+	i = 0;
+	while (input_str[*current_pos] && input_str[*current_pos] != token_hint) // TODO: replace token hint (interger value) with character at that posiiton in the stirng!
+	{
+		(*current_pos)++;
+		i++;
+	}
+	new_token->type = S_QUOTE;
 }
