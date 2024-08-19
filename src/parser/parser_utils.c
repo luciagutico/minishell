9d35fe84c3f308ea98a5111c03e8ebe1527923b0
jguacide <guacidejohanna@gmail.com>
2024-08-19 15:10:06 +0200

    parsing wip: get_redirection_file now split into two functions for norm

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -45,12 +45,34 @@ t_redirection_type	get_redirection_type(char *str)
 	return (0);
 }
 
+char	*get_file_name(char *file, int end_str, int token_nbr, t_token **current_token)
+{
+	int	i;
+	int	j;
+	int str_i;
+
+	i = 0;
+	j = 0;
+	while (j < token_nbr && i < end_str)
+	{
+		str_i = 0;
+		while ((*current_token)->str[str_i] != '\0')
+		{
+			file[i] = (*current_token)->str[str_i];
+			str_i++, i++;
+		}
+		*current_token = (*current_token)->next;
+		j++;
+	}
+	file[i] = '\0';
+	return (file);
+}
+
 char	*get_redirection_file(t_token **current_token)
 {
 	char *file;
 	int i;
 	int j;
-	int str_i;
 	int end_str;
 	int token_nbr;
 	t_token	*token_pos;
@@ -70,21 +92,22 @@ char	*get_redirection_file(t_token **current_token)
 	file = calloc(i + 1, sizeof(char)); //TODO: protect && separate second part in a different function
 	end_str = i;
 	token_nbr = j;
-	i = 0;
-	j = 0;
-	str_i = 0;
-	while (j < token_nbr && i < end_str)
-	{
-		str_i = 0;
-		while ((*current_token)->str[str_i] != '\0')
-		{
-			file[i] = (*current_token)->str[str_i];
-			str_i++, i++;
-		}
-		*current_token = (*current_token)->next;
-		j++;
-	}
-	file[i] = '\0';
+	file = get_file_name(file, i, j, current_token);
+	// i = 0;
+	// j = 0;
+	// str_i = 0;
+	// while (j < token_nbr && i < end_str)
+	// {
+	// 	str_i = 0;
+	// 	while ((*current_token)->str[str_i] != '\0')
+	// 	{
+	// 		file[i] = (*current_token)->str[str_i];
+	// 		str_i++, i++;
+	// 	}
+	// 	*current_token = (*current_token)->next;
+	// 	j++;
+	// }
+	// file[i] = '\0';
 	return (file);
 }
 
@@ -98,6 +121,7 @@ void	fill_redirect_out(t_command *new_command, t_token **current_token)
 	{
 		out->redir_type = get_redirection_type((*current_token)->str);
 		out->file = get_redirection_file(current_token);
+		// out->fd = get_fd_file(current_token);
 	}
 	new_command->out = out;
 	printf("redirection file = %s\n", new_command->out->file);
